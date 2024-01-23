622b2e0a6792367821cdc3502dd5c86b3c9ba2ee
kaimo313 <kaimo313@foxmail.com>
2024-01-23 13:53:34 +0800

    feat：【002】2620. 计数器

---
+++ doc/【002】2620. 计数器.js
@@ -0,0 +1,17 @@
+// 给定一个整型参数 n，请你编写并返回一个 counter 函数。
+// 这个 counter 函数最初返回 n，每次调用它时会返回前一个值加 1 的值 ( n ,  n + 1 ,  n + 2 ，等等)。
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    return function () {
+        return n++;
+    };
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
